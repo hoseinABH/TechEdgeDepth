8691e228fc6119d4ac1282d4fe592978d62cd417
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-12-29 21:50:39 +0330

    adding sw

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/practicaldev/image/fetch/',
   },
-};
+});
--- src/defaultTemplate/index.tsx
+++ src/defaultTemplate/index.tsx
@@ -14,14 +14,44 @@ function DefaultTemplate({ maxWidth, children, title }: DefaultTemplateProps) {
     <div className={theme === 'dark' ? 'dark' : 'light'}>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.png" />
         <base target="_blank" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#9f00a7" />
+        <meta name="theme-color" content="#111827" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
           content="a website for reading and being up to date in last tech tricks..."
         />
         <meta property="og:title" content={title} key="title" />
+        <meta property="og:site_name" content="DeepDev" />
+        <meta property="og:url" content="https://deep-eta.vercel.app/" />
+        <meta
+          property="og:description"
+          content="a website for reading and being up to date in last tech tricks..."
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="/favicon.png"></meta>
       </Head>
       <div className="bg-gray-100 dark:bg-gray-900 min-h-screen  h-full pb-8">
         <NavBar />
